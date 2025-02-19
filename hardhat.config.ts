ccb3bf3066c0b9d514fe0bce11ca38274075e77c
Asquarep <asquarep@yahoo.com>
2025-02-19 19:23:41 +0100

    v3 test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
+        url: "https://eth-mainnet.g.alchemy.com/v2/fi4KJ97Uo8YTlaNTnq9z67XyXBKnK6di",
       },
     },
   },
